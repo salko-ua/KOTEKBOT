f7b231c647eba25688dd2581c6b4ba757a58f0cc
salkoua <chips69jamil69game@gmail.com>
2023-02-12 23:19:06 +0200

    start polling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram.utils.executor import start_webhook
+from aiogram.utils import executor
 from data_base.controller_db import bd_Start
 import os
 from create_bot import bot,dp
@@ -17,25 +18,27 @@ async def regiseter_handlers():
     client.register_handler_client(dp)
 
 async def on_startup(dp):
-    await bot.set_webhook(APP_URL)
+    #await bot.set_webhook(APP_URL)
     await regiseter_handlers()
     print("BOT ONLINE")
     bd_Start()
     
 
 async def on_shutdown(dp):
-    await bot.delete_webhook()
+    #await bot.delete_webhook()
+    pass
 
 
 def start_bot():
-    start_webhook(
-        dispatcher=dp,
-        webhook_path='',
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=False,
-        host = '0.0.0.0',
-        port = int(os.environ.get("WEBHOOK_PORT", 8000)))
+    executor.start_polling(dp, skip_updates = False)
+    #start_webhook(
+    #    dispatcher=dp,
+    #    webhook_path='',
+    #    on_startup=on_startup,
+    #    on_shutdown=on_shutdown,
+    #    skip_updates=False,
+    #    host = '0.0.0.0',
+    #    port = int(os.environ.get("WEBHOOK_PORT", 8000)))
 
 
 
