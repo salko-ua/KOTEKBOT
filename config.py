9edfafe8518c674d071be1b278c4e2071896d06f
salkoua <chips69jamil69game@gmail.com>
2023-03-28 22:06:08 +0300

    release 1.6e9

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ super_admin_ura = os.environ.get("sadminura", 862361179)
 token_alert = os.environ.get("ALERT_TOKEN", "")
 token_sentry = os.environ.get(
     "token_sentry",
-    "",
+    "https://622c27cfc84b46119192ff14073e2df9@o4504669478780928.ingest.sentry.io/4504669483827200",
 )
 # token_alert
