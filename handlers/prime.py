afae2d6b1534eb6f824f18c8ee06e8443fd4efc9
salkoua <chips69jamil69game@gmail.com>
2023-04-09 00:25:27 +0300

    release 1.7e2

--- handlers/prime.py
+++ handlers/prime.py
@@ -5,15 +5,21 @@ from aiogram.dispatcher.filters import Text
 
 async def text_save(message: types.Message):
     if await user_exists_sql(message.from_user.id):
-        link = message.text[7::]
-        groups = await see_group_for_user_id(message.from_user.id)
-        await add_text_sql(link, groups)
-        await message.answer("Успішно додано!")
-    if await teachers_exists_sql(message.from_user.id):
-        link = message.text[7::]
-        groups = await see_group_for_teach_id(message.from_user.id)
-        await add_text_sql(link, groups)
-        await message.answer("Успішно додано!")
+        if message.text[7::] < 2:
+            await message.answer("текст закороткий")
+        else:
+            link = message.text[7::]
+            groups = await see_group_for_user_id(message.from_user.id)
+            await add_text_sql(link, groups)
+            await message.answer("Успішно додано!")
+    elif await teachers_exists_sql(message.from_user.id):
+        if message.text[7::] < 2:
+            await message.answer("текст закороткий")
+        else:
+            link = message.text[7::]
+            groups = await see_group_for_teach_id(message.from_user.id)
+            await add_text_sql(link, groups)
+            await message.answer("Успішно додано!")
     else:
         await message.answer("Ви не зареєстровані у групах")
 
